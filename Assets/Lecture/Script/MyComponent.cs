3537f02ff5433688289fdf5a4c5452adaabc4fab
SubNi <spal1251@gmail.com>
2016-09-07 12:38:07 +0900

    Day #2 수업
    - SerializedObject 활용
    - Scene 저장 알림 기능 추가
    - EditorWindow 만들기
    - Menu 만들기

--- Assets/Lecture/Script/MyComponent.cs
+++ Assets/Lecture/Script/MyComponent.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[AddComponentMenu("My Menu/My Component Script")] //컴포넌트 스크립트는 무조건 컴포넌트에..
 public class MyComponent : MonoBehaviour {
 
-    public int intVariable;
-    public float floatVariable;
+    public int intNum;
+    public float floatNum;
+ //   public GameObject gameObjectList;
 
 
 	// Use this for initialization
@@ -17,20 +19,21 @@ public class MyComponent : MonoBehaviour {
 	
 	}
 
-    public int IntVar
+    public int IntVar // Propety!
     {
         get
         {
-            return intVariable;
+            return intNum;
         }
         set
         {
-            intVariable = value;
+            intNum = value;
         }
     }
 
     public void DoSomething()
     {
-        intVariable++;
+        intNum++;
     }
+
 }
